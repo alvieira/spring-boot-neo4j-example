c0ef33c3f818928e66222f552bd1be4b156cfb07
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-03-29 20:47:51 -0300

    Example of Spring Boot with Neo4j

--- src/main/java/guru/springframework/domain/Product.java
+++ src/main/java/guru/springframework/domain/Product.java
@@ -1,7 +1,7 @@
 package guru.springframework.domain;
 
-import org.neo4j.ogm.annotation.GraphId;
 import org.neo4j.ogm.annotation.NodeEntity;
+import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @NodeEntity
 public class Product {
 
-    @GraphId
+    @Id
     private Long id;
     private String description;
     private BigDecimal price;
